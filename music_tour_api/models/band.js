586d13735536c5936883c6977948581f5a5544f7
Max Gamel <gamelmagdy2.0@gmail.com>
2023-01-16 11:46:29 -0500

    controllers

---
+++ music_tour_api/models/band.js
@@ -0,0 +1,42 @@
+
+const { Sequelize, DataTypes,  Model } = require('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+
+
+class Band extends Model{}
+
+Band.init({
+    band_id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true
+    },
+    name: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    genre: { 
+        type: DataTypes.TEXT, 
+        allowNull: false 
+    },
+    available_start_time: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+    end_time: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+}, {
+   
+        sequelize,                           
+        modelName: 'Band',
+        tableName: 'band',
+        timestamps: false
+    
+}) 
+
+
+
+
+module.exports = Band
